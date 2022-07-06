70079a69618a3ed8432c3bc99f2f0b6f87515db9
Victoria Dunlap <victoriacdunlap@gmail.com>
2022-07-06 19:52:42 -0400

    fixed fetch call and started the local storage

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,15 @@
-fetch('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=b4ba1fb1f1bea7f50275057fd60c746f', {
+var APIKey = 'b4ba1fb1f1bea7f50275057fd60c746f'
+
+var userInputCity 
+var userInputState
+var storage = window.localStorage.setItem(userInputCity, userInputState)
+
+// var searchInput = document.getElementById('searchBarInput')
+var searchButton = document.getElementById('searchButton')
+
+var 
+
+fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userInputCity + '&appid=' + APIKey, {
   method: 'GET', //GET is the default.
   credentials: 'same-origin', // include, *same-origin, omit
   redirect: 'follow', // manual, *follow, error
@@ -9,3 +20,7 @@ fetch('https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid
   .then(function (data) {
     console.log(data);
   });
+
+storage.addEventListener('click', () => { 
+  
+})
