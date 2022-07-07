f89e053dd87731b9df00aa63ec8a06664dd7796d
Victoria Dunlap <victoriacdunlap@gmail.com>
2022-07-07 07:57:49 -0400

    Set up local storage functions

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,13 @@
 var APIKey = 'b4ba1fb1f1bea7f50275057fd60c746f'
 
-var userInputCity 
-var userInputState
-var storage = window.localStorage.setItem(userInputCity, userInputState)
+var userInput 
+
+var storage = localStorage.setItem('userInput', JSON.stringify(userInput))
 
 // var searchInput = document.getElementById('searchBarInput')
 var searchButton = document.getElementById('searchButton')
 
-var 
-
-fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userInputCity + '&appid=' + APIKey, {
+fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + APIKey, {
   method: 'GET', //GET is the default.
   credentials: 'same-origin', // include, *same-origin, omit
   redirect: 'follow', // manual, *follow, error
@@ -22,4 +20,14 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userInputCity + '&a
   });
 
 storage.addEventListener('click', () => { 
-  
+})
+
+function createItem() {
+ localStorage.setItem('userInput')
+}
+createItem();
+
+function getInput() {
+  return localStorage.getItem('userInput')
+}
+console.log(getInput());
