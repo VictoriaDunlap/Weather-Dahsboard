1aa238ab4847402ed6239aceaab6ab8b1585f2f5
Victoria Dunlap <victoriacdunlap@gmail.com>
2022-07-07 10:02:36 -0400

    confirmed fetch call works. have skeleton of everything else

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,18 @@
 var APIKey = 'b4ba1fb1f1bea7f50275057fd60c746f'
+var jumbotron = document.getElementById("#jumbotron")
 
-var userInput 
+// var weatherURL = "'https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + APIKey"
+var userInput = 'atlanta'
 
 var storage = localStorage.setItem('userInput', JSON.stringify(userInput))
+console.log(userInput)
 
 // var searchInput = document.getElementById('searchBarInput')
 var searchButton = document.getElementById('searchButton')
+// + userInput + '&appid=' + 
+// function getWeather () 
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + APIKey, {
+fetch('https:api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + APIKey, {
   method: 'GET', //GET is the default.
   credentials: 'same-origin', // include, *same-origin, omit
   redirect: 'follow', // manual, *follow, error
@@ -17,17 +22,18 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid
   })
   .then(function (data) {
     console.log(data);
-  });
+});
+
 
 storage.addEventListener('click', () => { 
 })
 
 function createItem() {
- localStorage.setItem('userInput')
+ localStorage.setItem('atlanta')
 }
 createItem();
 
 function getInput() {
-  return localStorage.getItem('userInput')
+  return localStorage.getItem('atlanta')
 }
 console.log(getInput());
