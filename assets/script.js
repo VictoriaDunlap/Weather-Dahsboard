0389addcfd24952dd5b196f3faad91f5645f8c75
Victoria Dunlap <victoriacdunlap@gmail.com>
2022-07-07 17:41:58 -0400

    Final readme update. More tinkering with fetch functions

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,50 @@
 var APIKey = 'b4ba1fb1f1bea7f50275057fd60c746f'
 var jumbotron = document.getElementById("#jumbotron")
 
-// var weatherURL = "'https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + APIKey"
-var userInput = 'atlanta'
+var weatherURL = "'https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + APIKey"
+var userInput
 
 var storage = localStorage.setItem('userInput', JSON.stringify(userInput))
 console.log(userInput)
 
 // var searchInput = document.getElementById('searchBarInput')
 var searchButton = document.getElementById('searchButton')
+var searchBarInput = document.getElementById('searchBarInput')
+var ui = UIEvent()
+var fetch = fetch()
 // + userInput + '&appid=' + 
 // function getWeather () 
 
-fetch('https:api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + APIKey, {
+function getWeather(weatherURL) {
+  fetch('https:api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + APIKey, {
   method: 'GET', //GET is the default.
   credentials: 'same-origin', // include, *same-origin, omit
   redirect: 'follow', // manual, *follow, error
 })
   .then(function (response) {
-    return response.json();
+    return response.json()
   })
   .then(function (data) {
     console.log(data);
+    jumbotron.textContent = data.main.temp
 });
+}
 
+searchButton.addEventListener('click', () => { 
+  var value = searchBarInput.value
 
-storage.addEventListener('click', () => { 
+  fetch.getCurrent(value).then((data) => {
+    ui.populateUI(data)
+    ui.saveToLS(data)
+  })
 })
 
 function createItem() {
- localStorage.setItem('atlanta')
+ localStorage.setItem(userInput, JSON.stringify(userInput))
 }
 createItem();
 
 function getInput() {
-  return localStorage.getItem('atlanta')
+  return localStorage.getItem(userInput, JSON.stringify(userInput))
 }
 console.log(getInput());
