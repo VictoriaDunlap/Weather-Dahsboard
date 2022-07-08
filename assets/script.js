05b41a55084006646545cf7996683980bdd05c28
Victoria Dunlap <victoriacdunlap@gmail.com>
2022-07-08 07:12:45 -0400

    Cleaned up the code a little. Still working on local storage

--- assets/script.js
+++ assets/script.js
@@ -1,42 +1,48 @@
 var APIKey = 'b4ba1fb1f1bea7f50275057fd60c746f'
 var jumbotron = document.getElementById("#jumbotron")
-
 var weatherURL = "'https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + APIKey"
 var userInput
-
 var storage = localStorage.setItem('userInput', JSON.stringify(userInput))
 console.log(userInput)
-
-// var searchInput = document.getElementById('searchBarInput')
 var searchButton = document.getElementById('searchButton')
-var searchBarInput = document.getElementById('searchBarInput')
-var ui = UIEvent()
-var fetch = fetch()
-// + userInput + '&appid=' + 
-// function getWeather () 
 
-function getWeather(weatherURL) {
-  fetch('https:api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + APIKey, {
+
+var searchBarInput = document.getElementById('searchBarInput').value
+
+
+var ui = new UIEvent()
+var fetch = new fetch()
+var options = {
   method: 'GET', //GET is the default.
   credentials: 'same-origin', // include, *same-origin, omit
   redirect: 'follow', // manual, *follow, error
-})
+}
+
+// initial fetch function to communicate with the api 
+function getWeather(weatherURL) {
+  fetch(weatherURL, options)
   .then(function (response) {
     return response.json()
   })
-  .then(function (data) {
-    console.log(data);
-    jumbotron.textContent = data.main.temp
-});
+  .then(function (response) {
+    console.log(response);
+   
+  })
 }
+getWeather()
+
+
 
 searchButton.addEventListener('click', () => { 
-  var value = searchBarInput.value
-
-  fetch.getCurrent(value).then((data) => {
-    ui.populateUI(data)
-    ui.saveToLS(data)
-  })
+  // var value = searchBarInput.value
+  // fetch.getCurrent(value).then((data) => {
+  //   ui.populateUI(data)
+  //   ui.saveToLS(data)
+  //   jumbotron.textContent = data.main.temp
+  // })
+  () => {
+    searchBarInput
+  }
 })
 
 function createItem() {
@@ -45,6 +51,6 @@ function createItem() {
 createItem();
 
 function getInput() {
-  return localStorage.getItem(userInput, JSON.stringify(userInput))
+  return localStorage.getItem(userInput)
 }
 console.log(getInput());
