48c7d1d219e3a09f6bf63484060c463d10a38a68
Victoria Dunlap <victoriacdunlap@gmail.com>
2022-07-12 07:02:39 -0400

    Fixed fecth bug. local storage is set up

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,14 @@
-var APIKey = 'b4ba1fb1f1bea7f50275057fd60c746f'
+const APIKey = 'b4ba1fb1f1bea7f50275057fd60c746f'
 var jumbotron = document.getElementById("#jumbotron")
-var weatherURL = "'https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + APIKey"
-var userInput
-var storage = localStorage.setItem('userInput', JSON.stringify(userInput))
+var weatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + APIKey
+var userInput = ' '
+
 console.log(userInput)
 var searchButton = document.getElementById('searchButton')
 
 
-var searchBarInput = document.getElementById('searchBarInput').value
 
 
-var ui = new UIEvent()
-var fetch = new fetch()
 var options = {
   method: 'GET', //GET is the default.
   credentials: 'same-origin', // include, *same-origin, omit
@@ -21,36 +18,40 @@ var options = {
 // initial fetch function to communicate with the api 
 function getWeather(weatherURL) {
   fetch(weatherURL, options)
-  .then(function (response) {
-    return response.json()
-  })
-  .then(function (response) {
-    console.log(response);
-   
-  })
+    .then(function (response) {
+      return response.json()
+    })
+    .then(function (response) {
+      console.log(response);
+
+    })
 }
-getWeather()
 
-
-
-searchButton.addEventListener('click', () => { 
-  // var value = searchBarInput.value
-  // fetch.getCurrent(value).then((data) => {
-  //   ui.populateUI(data)
-  //   ui.saveToLS(data)
-  //   jumbotron.textContent = data.main.temp
-  // })
-  () => {
-    searchBarInput
-  }
-})
 
 function createItem() {
- localStorage.setItem(userInput, JSON.stringify(userInput))
+  localStorage.setItem(userInput, JSON.stringify(userInput.name))
 }
-createItem();
 
 function getInput() {
   return localStorage.getItem(userInput)
 }
-console.log(getInput());
+
+
+const getUsers = async () => {
+  var userInput = document.getElementById('searchBarInput').value
+  console.log(userInput)
+  var weatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + APIKey
+  await fetch(weatherURL)
+    .then(response => response.json())
+    .then(json => {
+      // localStorage.setItem(localStorage.length + 1, JSON.stringify(json))
+      localStorage.setItem(localStorage.length + 1, JSON.stringify(json.name))
+
+    })
+
+  for (let i = 0; i < localStorage.length; i++) {
+    let cityName = JSON.parse(localStorage.getItem(localStorage.key(i)))
+    console.log(cityName.name, cityName)
+  }
+
+}
